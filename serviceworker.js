4394bc1897c080a47b9bd56d82d83c648adf72a7
akashkasurde <akashkasurde1999@gmail.com>
2018-08-09 17:35:37 +0530

    initial commit

---
+++ serviceworker.js
@@ -0,0 +1,41 @@
+const cacheName = 'v1';
+
+//Install Event
+self.addEventListener('install', e => {
+  console.log('Service Worker');
+});
+
+// Activate Event
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== cacheName) {
+            console.log('Service Worker: Clearing Old Cache');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Call Fetch Event
+self.addEventListener('fetch', e => {
+  console.log('Service Worker: Fetching');
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        // Make clone of response
+        const resClone = res.clone();
+        // Open cache
+        caches.open(cacheName).then(cache => {
+          // Add response to the cache
+          cache.put(e.request, resClone);
+        });
+        return res;
+      })
+      .catch(err => caches.match(e.request).then(res => res))
+  );
+});
